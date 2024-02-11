a463539f3eff16af0149d4f3f3e7933b552b2ba4
sarvjeet.bhardwaj <sjitbh121993@gmail.com>
2024-02-11 15:53:43 +0530

    Final commit

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -12,7 +12,7 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = ""
+                mongo_db_url = "  "
                 #mongo_db_url = os.getenv(MONGO_DB_URL)
                 #if mongo_db_url is None:
                 #    raise Exception(f'Environment key:{MONGO_DB_URL} is not set.')
